6f69c0da538dd9502695176dcc8a9b401d794caf
Priyanshudev001 <sanjaydeo099@gmail.com>
2025-12-21 14:05:38 +0545

    2 tasks for milestone 1

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Journal_Application
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Journal Application" };
+        }
+    }
+}
